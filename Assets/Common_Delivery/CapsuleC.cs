3fe3e1d1ad9372d6e7ee0242573a5219a0185857
Shiro <eric.maestre@enti.cat>
2024-02-21 12:23:23 +0100

    last luttle push, test undone

--- Assets/Common_Delivery/CapsuleC.cs
+++ Assets/Common_Delivery/CapsuleC.cs
@@ -13,12 +13,31 @@ public struct CapsuleC
     #endregion
 
     #region CONSTRUCTORS
+    public CapsuleC (Vector3C positionA, Vector3C positionB, float radius)
+    {
+        this.positionA = positionA;
+        this.positionB = positionB;
+        this.radius = radius;
+    }
     #endregion
 
     #region OPERATORS
     #endregion
 
     #region METHODS
+    public bool isInside()
+    {
+        return false;
+    }
+    public bool Equals(CapsuleC other)
+    {
+        Vector3C AB = this.positionA - this.positionB;
+        Vector3C ABOther = other.positionA - this.positionB;
+        if(this.radius == other.radius || AB.Magnitude() == ABOther.Magnitude())
+            return true;
+        else 
+            return false;
+    }
     #endregion
 
     #region FUNCTIONS
