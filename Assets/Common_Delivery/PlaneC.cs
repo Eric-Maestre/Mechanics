e2cdf7f7f2fc6521f8f6b5d2362fc50dc8714447
Miquel Cortijo Martínez <miquel.cortijo@enti.cat>
2024-02-22 17:51:44 +0100

    Initial testing ended

--- Assets/Common_Delivery/PlaneC.cs
+++ Assets/Common_Delivery/PlaneC.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Net.NetworkInformation;
+using System.Numerics;
+using static UnityEngine.Rendering.DebugUI;
 
 [System.Serializable]
 public struct PlaneC
@@ -64,6 +66,19 @@ public struct PlaneC
     }
     public Vector3C IntersectionWithLine(LineC line)
     {
+        //Give the equation of the plane with normal vector (10, 8, 3) that contains the point (10, 5, 5):
+        //    Recall that the scalar form of the equation of a plane with a normal vector n = (a, b, c),
+        //    that contains the point(x0, y0, z0), is a(x-x0) + b(y - y0) + c(z - z0) = 0.
+        //Thus, substituting the values for the given normal vector(10, 8, 3) and point(10, 5, 5), we have
+        //    10(x - 10) + 8(y - 5) + 3(z - 5) = 0
+        //    10x - 100 + 8y - 40 + 3z - 15 = 0
+        //    10x + 8y + 3z - 155 = 0
+        //    Thus, the general form of the equation of the plane with normal vector(10, 8, 3) that contains
+        //    the point(10, 5, 5) is 10x + 8y + 3z - 155 = 0
+
+        //PlaneC.normal.x(x - PlaneC.position.x) + PlaneC.normal.y(y - PlaneC.position.y) + PlaneC.normal.z(z - PlaneC.position.z) = 0
+        //PlaneC.normal.x*x + PlaneC.normal.y*y + PlaneC.normal.z*z
+        //- PlaneC.normal.x*PlaneC.position.x - PlaneC.normal.y*PlaneC.position.y - PlaneC.normal.z*PlaneC.position.z = 0
         return new Vector3C();
     }
     public bool Equals(PlaneC other)
--- Assets/Tests/Test1.cs
+++ Assets/Tests/Test1.cs
@@ -18,6 +18,7 @@ public class Test1 : MonoBehaviour
     [SerializeField]
     PlaneC planeC;
     private Vector3C nearestPoint;
+    private Vector3C intersectionWithLine;
 
     // Update is called once per frame
     void Update()
@@ -25,9 +26,10 @@ public class Test1 : MonoBehaviour
         //nearestPoint = lineC.NearestPointToPoint(point);
         CustomDebug.Print(point, 5);
         nearestPoint = planeC.NearestPoint(point);
-        //CustomDebug.Print(lineC, colorRed, false);
-        
+        CustomDebug.Print(lineC, colorRed, false);
+        intersectionWithLine = planeC.IntersectionWithLine(lineC);
         CustomDebug.Print(nearestPoint, 5);
+        CustomDebug.Print(intersectionWithLine, 5);
         Debug.DrawLine(point.ToUnity(),nearestPoint.ToUnity(), Color.magenta, 5f);
         Debug.Log(nearestPoint.x);
         Debug.Log(nearestPoint.y);
